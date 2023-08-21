80c054dcdcb549e6df764f19c4801217251dbd76
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2023-08-21 08:47:15 +0300

    db updateed

--- products/views.py
+++ products/views.py
@@ -4,37 +4,15 @@ from .models import Product
 from django.contrib.auth.forms import UserCreationForm
 from .forms import CreateUserForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login as auth_login, logout
+from django.views.decorators.csrf import csrf_protect
+
 # Create your views here.
 
 
 def index(request):#main page of the app
     products = Product.objects.all()
     return render(request, 'index.html', {'products': products})
-
-
-def login(request):
-
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password )
-    
-        if user is not None:
-            login(request, user)
-            return redirect('homepage')
-        else:
-            messages.info(request, 'Username OR password is incorrect')
-
-    
-
-    context = {}
-    return render(request, 'login.html', context)
-
-def homepage(request):
-    context = {}
-    return render(request, 'homepage.html', context)
 
 def register(request):
     form = CreateUserForm()
@@ -46,8 +24,33 @@ def register(request):
             user = form.cleaned_data.get('username')
             messages.success(request, "Account was created for " + user)
             return redirect('login')
+        
 
             
 
     context = {'form':form}
     return render(request, 'register.html', context)
+
+@csrf_protect
+def login(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password )
+        
+        if user is not None:
+            auth_login(request, user)
+            return redirect('homepage')
+        else:
+            messages.error(request, 'Username OR password is incorrect')
+    
+
+    context = {}
+    return render(request, 'login.html', context)
+
+def homepage(request):
+    context = {}
+    return render(request, 'homepage.html', context)
+
