f654d351d7a8b68da0f2c9f0b6b954a18d649e30
Tnsae <tnsae.alemayehu@bitscollege.edu.et>
2023-08-24 13:30:55 +0300

    got admin to show categories on admin panel

--- products/admin.py
+++ products/admin.py
@@ -1,13 +1,53 @@
+# from django.contrib import admin
+# from .models import Product, Offer
+# # Register your models here.
+
+# class OfferAdmin(admin.ModelAdmin):
+#     list_display = ('code', 'description', 'discount' )
+
+
+# class ProductAdmin(admin.ModelAdmin):
+#     list_display = ('name', 'price', 'stock')
+
+# admin.site.register(Product, ProductAdmin)
+# admin.site.register(Offer, OfferAdmin) 
+
+from django import forms
 from django.contrib import admin
-from .models import Product, Offer
-# Register your models here.
+from mptt.admin import MPTTModelAdmin
 
-class OfferAdmin(admin.ModelAdmin):
-    list_display = ('code', 'description', 'discount' )
+from .models import(
+    Category,
+    Product,
+    ProductImage,
+    ProductSpecification,
+    ProductSpecificationValue,
+    ProductType,
+)
 
+admin.site.register(Category, MPTTModelAdmin) 
 
+class ProductSpecificationInline(admin.TabularInline):
+    model = ProductSpecification
+
+@admin.register(ProductType)
+class ProductTypeAdmin(admin.ModelAdmin):
+    inlines=[
+        ProductSpecificationInline
+    ]
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+
+class ProductSpecificationValueInline(admin.TabularInline):
+    model = ProductSpecificationValue
+
+@admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price', 'stock')
+    inlines= [
+        ProductSpecificationValueInline,
+        ProductImageInline,
+    ]
 
-admin.site.register(Product, ProductAdmin)
-admin.site.register(Offer, OfferAdmin) 
+
+
