945f6a3a1b9bfb66abf60da5ca5320c34ec3a8df
ArsemaYemiru <139496144+ArsemaYemiru@users.noreply.github.com>
2023-09-25 17:10:59 +0300

    added some tests and changed the design on the favorites page

--- products/tests.py
+++ products/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from models import ProductImage
+from models import Profile
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
+
+
+class ProductImageTestCases(TestCase):
+    def test_char(self):
+        test_ProductImage = ProductImage(product="", image="")
+
+        with self.assertRaises(ValidationError):
+            test_ProductImage.full_clean()
+
+
+class ProfileTestCases(TestCase):
+    def test_char(self):
+        test_Profile = Profile(username="", bio="", email="")
+
+        with self.assertRaises(ValidationError):
+            test_Profile.full_clean()
